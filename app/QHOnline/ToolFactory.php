aa968a5db5abff5312a72c1ebb76c9c7f81b60f5
vovbathang <thangnb@smartosc.com>
2019-01-30 10:33:13 +0700

    Edit js

--- app/QHOnline/ToolFactory.php
+++ app/QHOnline/ToolFactory.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\QHOnline;
 
-class ToolFactory {
-    public function getThumbnail($filename, $suffix = '_thumb') {
+class ToolFactory
+{
+    public function getThumbnail($filename, $suffix = '_thumb')
+    {
         if ($filename) {
 //            2017-04/b2d497b69515658e67d80d135b7d0b54.png
             return preg_replace("/(.*)\.(.*)/i", "$1{$suffix}.$2", $filename);
@@ -10,7 +13,8 @@ class ToolFactory {
         return '';
     }
 
-    public function getCurrencyVN($number, $symbol = 'VND', $isPrefix = false) {
+    public function getCurrencyVN($number, $symbol = 'VND', $isPrefix = false)
+    {
         if ($isPrefix) {
             return $symbol . number_format($number, 0, ',', '.');
         } else {
